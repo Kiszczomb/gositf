b502baf806b085e7d7748405912ab901f27287f8
Kacper Kiszczomb <kacper.kishchomb@gmail.com>
2018-11-15 11:47:22 +0100

    Add blink.js

---
+++ blink.js
@@ -0,0 +1,19 @@
+var GPIO = require('onoff').GPIO;
+var LED = new GPIO(4, 'out');
+var blinkInterval = setInterval(blinkLED, 250);
+
+function blinkLED(){
+    if (LED.readSync() === 0) {
+        LED.writeSync(1);
+    } else {
+        LED.writeSync(0);
+    }
+}
+
+function endBlink() {
+    clearInterval(blinkInterval);
+    LED.writeSync(0);
+    LED.unexport();
+}
+
+setTimeout(endBlink, 5000);
