91d1804801ffb9614f967db58e0ba4f498c25eae
Kacper Kiszczomb <kacper.kishchomb@gmail.com>
2018-11-15 13:11:03 +0100

    Mod webserver.js add io

--- webserver.js
+++ webserver.js
@@ -1,5 +1,6 @@
 var http = require('http').createServer(handler); //require http server, and create server with function handler()
 var fs = require('fs'); //require filesystem module
+var io = require('socket.io')(http) //require socket.io module and pass the http object (server)
 
 http.listen(8080); //listen to port 8080
 console.log("listen to port 8080")
