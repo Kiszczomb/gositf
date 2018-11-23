420facbd3eaa7269412ae4f6686025bef78e1c96
Kacper Kiszczomb <kacper.kishchomb@gmail.com>
2018-11-23 12:52:32 +0100

    Cleaned index.js & index.html

--- index.js
+++ index.js
@@ -7,28 +7,6 @@ app.use(bodyParser.json());
 
 app.use(express.static(__dirname + '/public'));
 
-// const cars = [
-//     { id: 1, brand: 'Ford', model: 'Focus'},
-//     { id: 2, brand: 'Śkoda', model: 'Octavia'},
-//     { id: 3, brand: 'Chrysler', model: 'Voyager'},
-//     { id: 4, brand: 'Nissan', model: 'Note'},
-//     { id: 5, brand: 'Wartburg', model: '1.3'}
-// ]
-
-// app.get('/', (req, res) => {
-//     res.send("Dobry wieczór")
-// })
-
-// app.get('/api/cars/:id', (req, res) => {
-//     const car = cars.find(c => c.id === parseInt(req.params.id))
-//     if(!car) res.status(404).send('Car with given ID not found')
-//     res.send(car)
-// })
-
-// app.get('/api/cars', (req, res) => {
-//     res.send(cars)
-// })
-
 app.get('/', (req, res) => res.sendFile(__dirname + "/index.html"))
 
 app.post('/button', (req, res) => {
